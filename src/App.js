7c9f9ed98286fe3a9aff240397982f89dd3e3fdb
Ali Haider <alihaiderx736@gmail.com>
2021-03-07 20:05:58 +0000

    Search React Component Created and Added to App.js , state variables declared and handle input and button functions created.
    - Search React Component created.
    - use state variables declared.
    - handle input and handle button basic functions created.
    - search component added to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import SearchComponent from './SearchComponent';
+import { useState, useEffect } from "react";
 
 function App() {
+ 
+  let [searchInput, setSearchInput] = useState("");
+
+  function handleInput(e) {
+    setSearchInput(e.target.value);
+    console.log(e.target.value);
+  }
+
+  function handleButton(e){
+    let input = document.getElementById("input");
+    e.preventDefault();
+    setSearchInput(input.value);
+    console.log(input.value);
+  }
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +33,8 @@ function App() {
       </header>
       <section className="App-section">
         <h1>BT React Code Test - by John Doe - #/01/21</h1>
+        <SearchComponent handleInput={handleInput} handleButton={handleButton}/>
+
       </section>
       <footer className="App-footer">
         <h5> © BT 2021</h5>
