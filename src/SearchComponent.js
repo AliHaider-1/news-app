d905b55100967f4a37e197b33d758270d07bd07c
Ali Haider <alihaiderx736@gmail.com>
2021-03-09 01:15:40 +0000

    - styling of input and button updated.

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -6,14 +6,23 @@ const SearchComponent = ({handleButton}) => {
   return (
     <div>
       <form>
-        <input
-          type="search"
-          id="input"
-          placeholder="Search News Articles"
-        ></input>
-        <button type="submit" onClick={handleButton}>
-          Search
-        </button>
+        <div class="container input-group">
+          <input
+            type="search"
+            class="form-control"
+            id="input"
+            placeholder="Search News Articles"
+          ></input>
+          <div class="input-group-append">
+            <button
+              class="btn btn-primary"
+              type="submit"
+              onClick={handleButton}
+            >
+              Search
+            </button>
+          </div>{" "}
+        </div>
       </form>
     </div>
   );
