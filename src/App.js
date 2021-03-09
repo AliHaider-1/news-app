cdda027d0fb685da5f82e680981bd2858f75b37f
Ali Haider <alihaiderx736@gmail.com>
2021-03-09 00:29:04 +0000

    Search Logic implemented. Title Added.  unnecessary code removed.
    - Search Logic implemented.
    - limit applied to the news articles list  to 10 articles.
    - alt attribute added to the image for functional accessibility for screen readers.
    - unnecessary prop handle input removed.
    - unnecessary function handle input removed.
    - unnecessary code removed.

--- src/App.js
+++ src/App.js
@@ -5,59 +5,57 @@ import Card from "react-bootstrap/Card";
 
 
 function App() {
-  let [searchInput, setSearchInput] = useState("");
 
  let [state,setState] =useState("");
+ let [title, setTitle] = useState("Top 10 Headlines of US");
 
-
-  function handleInput(e) {
-    setSearchInput(e.target.value);
-    console.log(e.target.value);
-  }
 
    let input = document.getElementById("input");
   
    function handleButton(e) {
     e.preventDefault();
-    setSearchInput(input.value);
+    setUrl(
+      `https://newsapi.org/v2/everything?q=${input.value}&apiKey=533137d100fe476da2cf7915337b04a3`
+    );
+    setTitle(`Showing news articles related to ${input.value}`)
     console.log(input.value);
    }
 
     const [isLoading, setIsLoading] = useState(true);
+    let [url, setUrl] = useState("https://newsapi.org/v2/top-headlines?country=us&apiKey=3ce1a4e5cf594ca7bd6ae27af898c3a1");
  
     const [dataArr, setDataArr] = useState("");
 useEffect(() => {
-    fetchData();
-  }, []);
-//fetching the api
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=3ce1a4e5cf594ca7bd6ae27af898c3a1"
-      );
+      const response = await fetch(url);
       if (response === undefined) {
         setIsLoading(true);
-      } 
-       else if (response.status >= 400) {
-         setIsLoading(true);
-         setState(response.status + " Bad Request ");
-       } 
-        else {
-         const data = await response.json();
-         setDataArr(data.articles);
-         console.log(data);
-         setIsLoading(false);
-       }
-     
+      } else if (response.status >= 400) {
+        setIsLoading(true);
+        setState(response.status + " Bad Request ");
+      } else {
+        const data = await response.json();
+        setDataArr(data.articles);
+        console.log(data);
+        setIsLoading(false);
+      }
     } catch (err) {
       setState(err.message);
-     console.log(err.message);
-     
+      console.log(err.message);
     }
   };
-  
+  fetchData();
+}, [url]);
+        const list = dataArr;
+        const size = 10;
+        const items = list.slice(0, size);
         
-      
+        
+          
+          
+        
+
   return (
     <div className="App">
       <header className="App-header">
@@ -70,31 +68,24 @@ useEffect(() => {
       </header>
       <section className="App-section">
         <h1>BT React Code Test - by John Doe - #/01/21</h1>
-
-        <SearchComponent
-          handleInput={handleInput}
-          handleButton={handleButton}
-        />
+        <h4>{title}</h4>
+        <SearchComponent handleButton={handleButton} />
 
         {isLoading ? (
-          
           <div>
             Loading ...
             <h3> ERROR:_ {state} </h3>
           </div>
-          
         ) : (
- <div className="row m-3 p-2">
-            {dataArr.map((user) => (
-              <div>
-                <Card
-                  key={user.name}
-                  style={{ width: "20rem", height: "40rem" }}
-                >
+          <div className="row m-3 p-2">
+            {items.map((user,index) => (
+              <div key={index}>
+                <Card style={{ width: "20rem", height: "40rem" }}>
                   <Card.Img
                     variant="top"
                     src={user.urlToImage}
                     style={{ height: "12rem" }}
+                    alt={user.title}
                   />
                   <Card.Title>
                     <strong>{user.title}</strong>
